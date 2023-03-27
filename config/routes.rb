bcb5417579c0ce4cf39729a1cbcf9c80289781f8
GUANTAI001 <jpguantai@gmail.com>
2023-03-27 17:17:10 +0300

    init:

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :destnations
   resources :destinations
   resources :usrs
   resources :payments
--- db/migrate/20230327141614_create_destnations.rb
+++ db/migrate/20230327141614_create_destnations.rb
@@ -1,6 +1,6 @@
-class CreateDestinations < ActiveRecord::Migration[6.1]
+class CreateDestnations < ActiveRecord::Migration[6.1]
   def change
-    create_table :destinations do |t|
+    create_table :destnations do |t|
       t.string :name
       t.string :image
       t.string :location
