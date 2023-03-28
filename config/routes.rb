6f0038e096575a4a71532f60aac80064c8929b04
Francisca <117682018+Francieorwa@users.noreply.github.com>
2023-03-28 22:42:01 +0300

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  resources :pays
   resources :books
   resources :destnations
-  resources :destinations
   resources :usrs
-  resources :payments
-  resources :bookings
-  resources :users
+  resources :pays
   # resources :users, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get"/", to: "destinations#index"
+  # get"/", to: "destinations#index"
+  get "pays", to: "pay#index"
+  get "pays/:id", to: "pay#show"
+  post '/pays', to: 'pays#create'
+
+  get "pesapal/callback", to: "pays#pesapal_callback"
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,16 @@
 
 # User.create(name: "guantai" , email: "jp@gmail.com", password: "1234")
 
-Destination.create(name: "Malindi", image: " xyz", location: "description", description: "travel safe", price: "1000")
+# Destination.create(name: "Malindi", image: " xyz", location: "description", description: "travel safe", price: "1000")
+# create a sample book
+# create a sample book
+# create a sample book
+destnation = Destnation.create(name: "New York City")
+usr = Usr.create(name: "John Doe", email: "john@example.com", password_digest: "password")
+book = Book.create(phone: "123-456-7890", date: "2023-04-01", capacity: 2, destnation_id: destnation.id, usr_id: usr.id)
+
+# create a new payment
+pay = Pay.create(amount: 9.99, method: "PesaPal", status: "pending", book_id: 1, usr_id: 10)
+
+
+
